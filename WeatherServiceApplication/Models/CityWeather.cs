262cad44a46d29521ab5ea863173fbea5bb9db39
Siddhesh <sid.gawas@live.in>
2019-06-29 20:28:28 +0530

    Model change

--- WeatherServiceApplication/Models/CityWeather.cs
+++ WeatherServiceApplication/Models/CityWeather.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -49,7 +49,7 @@ namespace WeatherServiceApplication
     public class Wind
     {
         public double speed { get; set; }
-        public int deg { get; set; }
+        //public int deg { get; set; }
     }
 
     public class Clouds
